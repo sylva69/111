b4e6758cd5600175a2416f2e7b0b182c4d0c8330
20392 <20392@1k129s6.dune.edu>
2023-02-27 16:22:11 +0200

    3 действие

--- src/pkg4/pkg1/Main.java
+++ src/pkg4/pkg1/Main.java
@@ -18,6 +18,7 @@ public class Main {
         // TODO code application logic here
         System.out.println("Hello World");        
         System.out.println("Hello Hello");
+        System.out.println("Hello Helloааа");
     }
     
 }
